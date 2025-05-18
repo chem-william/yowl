3034a5ab0c137a4fadbefcc493cf55078ca189db
William Bro-Jørgensen <brojorgensen@proton.me>
2025-05-18 19:39:34 +0200

    remove left over bracket_symbol

--- src/feature/mod.rs
+++ src/feature/mod.rs
@@ -1,6 +1,5 @@
 mod atom_kind;
 mod bond_kind;
-mod bracket_symbol;
 mod charge;
 mod configuration;
 mod element_ext;
@@ -10,7 +9,6 @@ mod virtual_hydrogen;
 pub use atom_kind::AtomKind;
 pub use atom_kind::Symbol;
 pub use bond_kind::BondKind;
-pub use bracket_symbol::BracketSymbol;
 pub use charge::Charge;
 pub use configuration::Configuration;
 pub use element_ext::ElementExt;
